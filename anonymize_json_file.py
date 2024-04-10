2b0e1ff544edd93f20165d03c872971ac9e1e415
Michela Iandoli <michela.iandoli@unicampus.it>
2024-04-10 12:09:23 +0200

    fixed error, refined code

--- anonymize_json_file.py
+++ anonymize_json_file.py
@@ -6,13 +6,21 @@ Created on Fri Apr  5 17:39:27 2024
 """
 
 #IMPORT PACKAGE I NEED
-#import  tkinter as tk
+import  tkinter as tk
 from tkinter import filedialog #The tkinter.filedialog module provides
 # classes and factory functions for creating file/directory selection windows.
 
 #FUNCTION TO GET FILE PATH
 def filepath ():
-    file_path = filedialog.askopenfilename()
+    window = tk.Tk()
+    window.wm_attributes('-topmost', 1)
+    window.withdraw()   # this supress the tk window
+
+    file_path = filedialog.askopenfilename(parent=window,
+                                  initialdir="",
+                                  title="Select A File",
+                                  filetypes = (("All files", "*"), ("Text files", "*.txt")))
+    #file_path = filedialog.askopenfilename()
     return file_path
 
 #FUNCTION THAT RETURNS A DICTIONARY THAT ASSOCIATE NAMES AND ID NUMBERS
@@ -20,25 +28,81 @@ def link(usernames_list,start_from=0):
     i=0
     link_dict={} #i start an empty dict to save each name and its corresponding ID number
     while i<len(usernames_list):
-        link_dict[usernames_list[i]]=str(i+start_from+1).zfill(10) #i put as key the name and as value the ID number
+        link_dict[usernames_list[i]]=str(i+start_from).zfill(10) #i put as key the name and as value the ID number
         i+=1
     return link_dict
 
 #FUNCTION TO DE-ANONYMIZE
 def deanon(explicit_list,associations):
-    procede_de_anon = input('If you want to de-anonymize the list again, press 1, otherwise press 0\n')
-    if procede_de_anon=='1':
-        for i in range(0,len(explicit_list)):
-            for k in associations.keys():
-                if explicit_list[i][1]==associations[k]:
-                    explicit_list[i][1]=k
-        print('the list is NOT anonymized')
-    else:
-        print('the list is anonymized')
+    for i in range(0,len(explicit_list)):
+        for k in associations.keys():
+            if explicit_list[i][1]==associations[k]:
+                explicit_list[i][1]=k
     
+#FUNCTION TO ANONYMIZE A SECOND LOG FILE
+def second_file(previous_user, previous_associations):
+    second_file=input('If you want to anonymize a second file press 1, otherwise press 0\n')
+    if second_file=='1':
+        print('Choose the file to anonymize from the window opened\n')
+        #import file
+        file=open(filepath())
+        explicit_list2=load(file) 
+        file.close()
+        #initialization
+        i=1
+        k=0
+        #user list extraction
+        user2=[] 
+        user2.append(explicit_list2[0][1]) 
+        while i<len(explicit_list2):
+            found=False 
+            while k<len(user2):
+                if explicit_list2[i][1]==user2[k]:
+                    found=True 
+                    break 
+                else: 
+                    k+=1
+            if not found: 
+                user2.append(explicit_list2[i][1]) 
+            i+=1 
+            k=0 
+        #searching new users not existing in the first user list
+        new_user=[]
+        for i in range(0,len(user2)):
+            already_existing=False
+            for k in range (0,len(user)):
+                if user[k]==user2[i]:
+                    already_existing=True
+                    break
+                else:
+                    k+=1
+            if not already_existing:
+                new_user.append(user2[i])
+            i+=1
+        #creating a second association dictionary for the new users
+        associations_new=link(new_user,len(user))
+        associations.update(associations_new)
+        #SUBSTITUTION OF NAMES WITH ID NUMBERS
+        i=0
+        k=0
+        while i<len(explicit_list2):
+            explicit_list2[i][1]=associations[explicit_list2[i][1]]
+            i+=1
+
+        #ELIMINATION OF THE "INVOLVED USER" COLUMN
+        i=0
+        while i<len(explicit_list2):
+            del explicit_list2[i][2]
+            i+=1
+        
+    else:
+        print('ok :(')
+    return explicit_list2
+
 
 #IMPORT THE EXPLICIT FILE
 from json import load
+print('Choose the file to anonymize from the window opened\n')
 file=open(filepath())
 explicit_list=load(file) #explicit_list is the list with the name still on it
 file.close()
@@ -79,54 +143,27 @@ while i<len(explicit_list):
     del explicit_list[i][2]
     i+=1
 
-#DE-ANONYMIZATION
-deanon(explicit_list,associations)
-
 #ANONIMIZATION OF A SECOND LOG FILE
+second_file_anonymized=second_file(user, associations)
 
-second_file=input('If you want to anonymize a second file press 1, otherwise press 0')
-if second_file=='1':
-    #import file
-    file=open(filepath())
-    explicit_list2=load(file) 
-    file.close()
-    #initialization
-    i=1
-    k=0
-    #user list extraction
-    user2=[] 
-    user2.append(explicit_list2[0][1]) 
-    while i<len(explicit_list2):
-        found=False 
-        while k<len(user2):
-            if explicit_list2[i][1]==user2[k]:
-                found=True 
-                break 
-            else: 
-                k+=1
-        if not found: 
-            user2.append(explicit_list2[i][1]) 
-        i+=1 
-        k=0 
-    #searching new users not existing in the first user list
-    new_user=[]
-    for i in range(0,len(user2)):
-        already_existing=False
-        for k in range (0,len(user)):
-            if user[k]==user2[i]:
-                already_existing=True
-                break
-            else:
-                k+=1
-            if not already_existing:
-                new_user.append(user2[i])
-            i+=1
-    #creating a second association dictionary for the new users
-    associations2=link(new_user,len(user))
-                
-    
+#DE-ANONYMIZATION
+procede_de_anon = input('If you want to de-anonymize one list again, press 1, otherwise press 0\n')
+if procede_de_anon=='1':
+    which_list=input('press 1 if you wanto to de-anonymize the first file, 2 for the second one\n')
+    if which_list=='1':
+        deanon(explicit_list,associations)
+    elif which_list=='2':
+        deanon(second_file_anonymized,associations)
+    else:
+        print('you press a wrong number')
+    print('the list is NOT anonymized anymore')
 else:
-    print('ok :(')
+    print('the list is still anonymized')
+
+
+
+
+
 
 
     
